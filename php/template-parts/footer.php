f693e2c7b0f80302f9d8e34cf53c054b47faec3b
Bryan-1065913 <1065913@hr.nl>
2024-06-08 14:36:26 +0200

    Added footer

---
+++ php/template-parts/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-auto py-3">
+    <div class="container text-center">
+        <span class="text-muted">© <?php echo date("Y"); ?> Listo. Alle rechten voorbehouden.</span>
+    </div>
+</footer>
+
+<script src="/assets/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="/assets/javascript/scripts.js"></script>
+</body>
+</html>
