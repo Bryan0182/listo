861e31e3cc3e5b1e7e5d8035ad8ce7a2dda69924
Bryan-1065913 <1065913@hr.nl>
2024-06-08 14:32:20 +0200

    Added local bootstrap

--- php/template-parts/head.php
+++ php/template-parts/head.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listo - <?php echo isset($pagetitle) ? $pagetitle : 'To Do'; ?></title>
     <link rel="icon" href="/assets/images/listo-logo-small.webp" type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/styles.css" rel="stylesheet">
-    <script src="../../assets/javascript/theme.js" defer></script>
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/styles.css" rel="stylesheet">
+    <script src="/assets/javascript/theme.js" defer></script>
 </head>
 <body>
